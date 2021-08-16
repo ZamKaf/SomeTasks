671ae95c5c8b497b9a3fc0d69dafe0c3fc43fa0a
barrens <barrens@okbsapr.ru>
2021-08-16 22:32:56 +0300

    Add task 180 missed array elements

---
+++ UniTests/UniTests/SummerTests2021/Task180FindMissedElements.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UniTests.SummerTests2021
+{
+    public class Task180FindMissedElements : ITest
+    {
+        public List<int> GetMissingNumbers(int[] arr)
+        {
+            var set = new HashSet<int>(arr);
+            return Enumerable.Range(1, arr.Length).Where(i => !set.Contains(i)).ToList();
+        }
+        
+        public void Run()
+        {
+            var testList = new[] { 4, 3, 2, 7, 8, 2, 3, 1, 1 };
+            foreach (var number in GetMissingNumbers(testList))
+            {
+                Console.Write($"{number} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
