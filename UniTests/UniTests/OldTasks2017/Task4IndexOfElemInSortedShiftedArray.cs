966951b53e5d92e77862ff104c36c29c011a8e8e
barrens <barrens@okbsapr.ru>
2021-08-18 00:06:57 +0300

    Add task 4 get elem from sorted shifted array

---
+++ UniTests/UniTests/OldTasks2017/Task4IndexOfElemInSortedShiftedArray.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace UniTests.OldTasks2017
+{
+    public class Task4IndexOfElemInSortedShiftedArray : ITest
+    {
+        public int GetIndex(int[] mas, int elem)
+        {
+            var minIndex = 0;
+            var maxIndex = mas.Length - 1;
+            while (maxIndex - minIndex > 1)
+            {
+                var midIndex = (minIndex + maxIndex) / 2;
+                var currentElem = mas[midIndex]; 
+                if (currentElem == elem)
+                    return midIndex;
+                if (currentElem < elem )
+                {
+                    minIndex = midIndex;
+                    continue;
+                }
+
+                if (elem >= mas[minIndex])
+                {
+                    maxIndex = midIndex;
+                }
+                else
+                {
+                    minIndex = midIndex;
+                }
+            }
+
+            if (elem == mas[maxIndex]) return maxIndex;
+            if (elem == mas[minIndex]) return minIndex;
+            throw new IndexOutOfRangeException();
+        }
+        
+        public void Run()
+        {
+            var array = new[] { 5, 6, 7, 8, 1, 2, 3, 4 };
+            var elem = 7;
+            Console.WriteLine($"index of {elem} in [{string.Join(", ", array)}] is {GetIndex(array, elem)}");
+        }
+    }
+}
